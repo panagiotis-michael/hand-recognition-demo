7e8008577297aac1837d0f3ac59d020f16cf0ca2
panagiotis-michael <panagiotis.michael133@gmail.com>
2022-09-22 20:03:15 +0300

    Creating window and adding some buttons.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 # This is a Hand Recognition Demo
+
+import PySimpleGUI as g
+
+
+g.change_look_and_feel('Dark')
+
+
+bt_color = 'black on yellow'
+bt_font = 'Inter'
+bt_size = (20,2)
+
+
+layout = [[g.Button('Turn On Camera', button_color = bt_color, size = bt_size , font = bt_font)],
+            [g.Button('Detect Hand', button_color = bt_color, size = bt_size, font = bt_font)],
+            [g.Button('Take Picture', button_color = bt_color, size = bt_size, font = bt_font)],
+            [g.Button('Predict Visual', button_color = bt_color, size = bt_size, font = bt_font)]]
+
+
+w , h = g.Window.get_screen_size()
+
+
+window = g.Window("Hand Recognition Demo", layout, size = (w // 2, h // 2))
+
+
+while True:
+    event, values = window.read()
+    if event == "Ex1" or event == g.WIN_CLOSED:
+        break
+window.close()
