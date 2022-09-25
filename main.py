3ce9551ec604170cc253946c672c89185f3b4c99
panagiotis-michael <panagiotis.michael133@gmail.com>
2022-09-25 18:05:26 +0300

    Added camera Frame

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # This is a Hand Recognition Demo
 
+
 import PySimpleGUI as g
 import cv2
+
 
 g.change_look_and_feel('Dark')
 
@@ -10,18 +12,26 @@ btn_color = 'black on yellow'
 btn_font = 'Inter'
 btn_size = (25,2)
 
+
 col1 = [[g.Frame('Choices',[
             [g.Button('Turn On Camera ON/OFF', button_color = btn_color, size = btn_size , font = btn_font)],
             [g.Button('Detect Hand ON/OFF', button_color = btn_color, size = btn_size, font = btn_font)],
             [g.Button('Take Picture', button_color = btn_color, size = btn_size, font = btn_font)],
             [g.Button('Predict Visual', button_color = btn_color, size = btn_size, font = btn_font)]
-                        ])]
+                        ])
+        ]
         ]
 
-col2 = []
+
+col2 = [[g.Frame('Camera',[
+            [g.Image(filename = '', key = 'image')]],
+            size = (800, 600))
+        ]
+        ]
+        
 
 
-layout = [[g.Column(col1, element_justification = 'c')]]
+layout = [[g.Column(col1, element_justification = 'c'), g.Column(col2, element_justification = 'c')]]
 
 
 window = g.Window("Hand Recognition Demo", layout)
@@ -31,4 +41,6 @@ while True:
     event, values = window.read()
     if event == "Ex1" or event == g.WIN_CLOSED:
         break
+
+
 window.close()
