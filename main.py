8e200ab4e3ff72b0550461e29c93af6143e693c4
panagiotis-michael <panagiotis.michael133@gmail.com>
2022-09-25 18:31:38 +0300

    Changed style and format

--- main.py
+++ main.py
@@ -5,33 +5,81 @@ import PySimpleGUI as g
 import cv2
 
 
-g.change_look_and_feel('Dark')
+g.change_look_and_feel("Dark")
 
 
-btn_color = 'black on yellow'
-btn_font = 'Inter'
-btn_size = (25,2)
+btn_color = "black on yellow"
+btn_font = "Inter"
+btn_size = (25, 2)
 
 
-col1 = [[g.Frame('Choices',[
-            [g.Button('Turn On Camera ON/OFF', button_color = btn_color, size = btn_size , font = btn_font)],
-            [g.Button('Detect Hand ON/OFF', button_color = btn_color, size = btn_size, font = btn_font)],
-            [g.Button('Take Picture', button_color = btn_color, size = btn_size, font = btn_font)],
-            [g.Button('Predict Visual', button_color = btn_color, size = btn_size, font = btn_font)]
-                        ])
-        ]
-        ]
+frame_font = "Inter"
 
 
-col2 = [[g.Frame('Camera',[
-            [g.Image(filename = '', key = 'image')]],
-            size = (800, 600))
-        ]
-        ]
-        
+col1 = [
+    [
+        g.Frame(
+            "Choices",
+            [
+                [
+                    g.Button(
+                        "Turn On Camera ON/OFF",
+                        button_color=btn_color,
+                        size=btn_size,
+                        font=btn_font,
+                    )
+                ],
+                [
+                    g.Button(
+                        "Detect Hand ON/OFF",
+                        button_color=btn_color,
+                        size=btn_size,
+                        font=btn_font,
+                    )
+                ],
+                [
+                    g.Button(
+                        "Take Picture",
+                        button_color=btn_color,
+                        size=btn_size,
+                        font=btn_font,
+                    )
+                ],
+                [
+                    g.Button(
+                        "Predict Visual",
+                        button_color=btn_color,
+                        size=btn_size,
+                        font=btn_font,
+                    )
+                ],
+            ],
+            font=frame_font,
+            border_width=3,
+        )
+    ]
+]
 
 
-layout = [[g.Column(col1, element_justification = 'c'), g.Column(col2, element_justification = 'c')]]
+col2 = [
+    [
+        g.Frame(
+            "Camera",
+            [[g.Image(filename="", key="image")]],
+            size=(800, 600),
+            font=frame_font,
+            border_width=3,
+        )
+    ]
+]
+
+
+layout = [
+    [
+        g.Column(col1, element_justification="c"),
+        g.Column(col2, element_justification="c"),
+    ]
+]
 
 
 window = g.Window("Hand Recognition Demo", layout)
